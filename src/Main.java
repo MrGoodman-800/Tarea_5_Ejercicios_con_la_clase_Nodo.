6e7b196c7fdcfd53880f2fc2b6667735bc1f8d3b
MrGoodman-800 <saulalexisariasgonzalez@gmail.com>
2026-09-16 22:05:24 -0600

    Codigo terminado

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,49 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Nodo<String> head = new Nodo<>();
+        head.setDato("Al");
+        head.setSiguiente(new Nodo<>("B"));
+        head.getSiguiente().setSiguiente(new Nodo<>("C"));
+        head.getSiguiente().getSiguiente().setSiguiente(new Nodo<>("De"));
+        head.getSiguiente().getSiguiente().getSiguiente().setSiguiente(new Nodo<>("Mc"));
+        head.getSiguiente().getSiguiente().getSiguiente().getSiguiente().setSiguiente(new Nodo<>("Zi"));
+        //1 Imprime el estado inicial completo de la lista
+        System.out.println("Estado unicial");
+        System.out.println(head);
+        //2 Imprime únicamente el dato almacenado en el primer nodo de la lista.
+        System.out.println("primer nodo");
+        System.out.printf(head.getDato());
+        //3 Imprime el estado completo del nodo ubicado en la última posición de la lista.
+        System.out.println();
+        System.out.println("nodo en la ultima posicion");
+        System.out.println(head.getSiguiente().getSiguiente().getSiguiente().getSiguiente().getSiguiente());
+        //4 Inserta un nuevo nodo con el valor "Fe" entre los nodos que contienen "De" y "Mc".
+        Nodo tme=new Nodo<>("Fe");
+        tme.setSiguiente(head.getSiguiente().getSiguiente().getSiguiente().getSiguiente());
+        head.getSiguiente().getSiguiente().getSiguiente().setSiguiente(tme);
+        // 5 Imprime el nuevo estado de la lista.
+        System.out.println();
+        System.out.println("nuevo estado");
+        System.out.println(head);
+        //6 Inserta un nuevo nodo con el valor "Zz" al final de la lista.
+        Nodo zef= new Nodo<>("Zz");
+        head.getSiguiente().getSiguiente().getSiguiente().getSiguiente().getSiguiente().getSiguiente().setSiguiente(zef);
+        //7 Imprime el nuevo estado de la lista.
+        System.out.println();
+        System.out.println("nuevo estado");
+        System.out.println(head);
+        //8 Inserta un nuevo nodo con el valor "Aa" al inicio de la lista, de modo que se convierta en el primer nodo.
+        Nodo jisus = new Nodo<>("Aa");
+        jisus.setSiguiente(head);
+        head = jisus;
+        // 9 Imprime el nuevo estado de la lista.
+        System.out.println();
+        System.out.println("nuevo estado");
+        System.out.println(head);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+
+
+
     }
 }
--- src/Nodo.java
+++ src/Nodo.java
@@ -1,4 +1,40 @@
-package PACKAGE_NAME;
+public class Nodo<T> {
+    private T dato;
+    private Nodo<T> siguiente;
 
-public class Nodo {
+    public Nodo() {
+    }
+
+    public Nodo(T dato) {
+        this.dato = dato;
+    }
+
+    public Nodo(T dato, Nodo<T> siguiente) {
+        this.dato = dato;
+        this.siguiente = siguiente;
+    }
+
+    public T getDato() {
+        return dato;
+    }
+
+    public void setDato(T dato) {
+        this.dato = dato;
+    }
+
+    public Nodo<T> getSiguiente() {
+        return siguiente;
+    }
+
+    public void setSiguiente(Nodo<T> siguiente) {
+        this.siguiente = siguiente;
+    }
+
+    @Override
+    public String toString() {
+        return "Nodo{" +
+                "dato=" + dato +
+                ", siguiente=" + siguiente +
+                '}';
+    }
 }
